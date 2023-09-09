0d80e076ef9ad5aceddbf42be3ec7a8087cd4b07
btezhu <burykin_ma@student.kings.edu.au>
2023-09-09 17:46:28 +1000

    Update indentation

--- users.sh
+++ users.sh
@@ -3,13 +3,13 @@
 set -e
 
 if [[ $# -lt 1 ]]; then
-    echo "No users file provided."
-    exit 1
+	echo "No users file provided."
+	exit 1
 fi
 users_file_path="$1"
 if [ ! -f $users_file_path ]; then
-    echo "The file $users_file_path does not exist."
-    exit 1
+	echo "The file $users_file_path does not exist."
+	exit 1
 fi
 
 users_commands_file_path="$2"
@@ -23,15 +23,15 @@ fi
 users_in_file=()
 declare -A users_encountered
 while IFS= read -r line; do
-    users_in_file+=("$line")
-    users_encountered["$line"]=""
+	users_in_file+=("$line")
+	users_encountered["$line"]=""
 done < <(grep . "${users_file_path}")
 
 # read users from /etc/passwd
 users_in_passwd=()
 while IFS= read -r line; do
 	if [[ "$line" != "#"* ]]; then # if the line doesn't start with a comment
-    	users_in_passwd+=("$line")
+		users_in_passwd+=("$line")
 	fi
 done < /etc/passwd
 
@@ -55,22 +55,22 @@ echo_command "passwd -l root"
 echo_command
 
 for user in "${users_in_passwd[@]}"; do
-    IFS=":"
-    read username encrypted_password user_id group_id user_id_info home_directory shell <<< "$user"
-    if [[ "$user_id" -lt "1000" ]]; then # system user
-        if [[ ! ("$shell" == "/bin/false" || "$shell" == "/usr/sbin/nologin" || "$shell" == "/usr/bin/false")]]; then
-            echo_todo "Set shell for user $username from $shell to /bin/false"
-        fi
-        if [ "$user_id" == "0" ] && [ "$username" != "root" ]; then
-            echo_todo "Set user_id for user $username to nonzero value"
-        fi
-        if [ "$group_id" == "0" ] && [ "$username" != "root" ]; then
-            echo_todo "Set user_id for user $username to nonzero value"
-        fi
-    fi
-    if [ "$encrypted_password" != "x" ]; then
-        echo_todo "Ensure user $username has an 'x' in /etc/passwd"
-    fi
+	IFS=":"
+	read username encrypted_password user_id group_id user_id_info home_directory shell <<< "$user"
+	if [[ "$user_id" -lt "1000" ]]; then # system user
+		if [[ ! ("$shell" == "/bin/false" || "$shell" == "/usr/sbin/nologin" || "$shell" == "/usr/bin/false")]]; then
+			echo_todo "Set shell for user $username from $shell to /bin/false"
+		fi
+		if [ "$user_id" == "0" ] && [ "$username" != "root" ]; then
+			echo_todo "Set user_id for user $username to nonzero value"
+		fi
+		if [ "$group_id" == "0" ] && [ "$username" != "root" ]; then
+			echo_todo "Set user_id for user $username to nonzero value"
+		fi
+	fi
+	if [ "$encrypted_password" != "x" ]; then
+		echo_todo "Ensure user $username has an 'x' in /etc/passwd"
+	fi
 	is_in_file=0
 	if [ "$username" == "root" ]; then
 		is_in_file=1
