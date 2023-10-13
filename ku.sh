c9d8a00db5e4bd69b225a8bfec198180f40ba5fd
btezhu <burykin_ma@student.kings.edu.au>
2023-10-14 02:15:20 +1100

    Add media, perms, and update

--- ku.sh
+++ ku.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# created 
+################################
+# created by btezhu
+################################
 
 set -e
 
@@ -212,6 +214,51 @@ do_groups() {
 	done
 }
 
+do_media() {
+	set +e -x
+	find / -name "*.xlsx" 2>/dev/null
+	find / -name "*.doc" 2>/dev/null 
+	find / -name "*.docx" 2>/dev/null
+	find / -name "*.exe" 2>/dev/null 
+	find / -name "*.mp3" 2>/dev/null 
+	find / -name "*.mov" 2>/dev/null 
+	find / -name "*.mp4" 2>/dev/null 
+	find / -name "*.avi" 2>/dev/null 
+	find / -name "*.mpg" 2>/dev/null 
+	find / -name "*.mpeg" 2>/dev/null
+	find / -name "*.flac" 2>/dev/null
+	find / -name "*.m4a" 2>/dev/null 
+	find / -name "*.flv" 2>/dev/null 
+	find / -name "*.ogg" 2>/dev/null 
+	find / -name "*.gif" 2>/dev/null 
+	find / -name "*.png" 2>/dev/null 
+	find / -name "*.jpg" 2>/dev/null 
+	find / -name "*.jpeg" 2>/dev/null
+	find / -name "*.txt" 2>/dev/null 
+	find / -name "*.tiff" 2>/dev/null
+	find / -name "*.bmp" 2>/dev/null 
+	find / -name "*.aac" 2>/dev/null 
+	find / -name "*.wav" 2>/dev/null 
+	find / -name "*.wma" 2>/dev/null 
+	find / -name "*.svg" 2>/dev/null 
+	find / -name "*.pdf" 2>/dev/null 
+	find / -name "*.zip" 2>/dev/null 
+	find / -name "*.iso" 2>/dev/null 
+	find / -name "*.rar" 2>/dev/null 
+	find / -name "*.jar" 2>/dev/null 
+	find / -name "*.msi" 2>/dev/null 
+	set -e +x
+}
+
+do_perms() {
+	set +e -x
+	find / -perm /4000 2>/dev/null
+	find / -perm /2000 2>/dev/null
+	find / -type f -perm -2 ! -type l -ls 2>/dev/null | grep -v "/proc/"
+	find / -nouser -o -nogroup 2>/dev/null
+	set -e +x
+}
+
 usage() {
 	echo "Usage: ku.sh <COMMAND> [OPTIONS]"
 	echo
@@ -219,7 +266,8 @@ usage() {
 	echo "    users <users-file> [<output-file>]"
 	echo "    groups <groups-file> [<output-file>]"
 	echo "    media"
-	echo "    permissions"
+	echo "    perms"
+	echo "    update"
 }
 
 if [[ $# -lt 1 ]]; then
@@ -257,6 +305,27 @@ elif [[ "$command" == "groups" ]]; then
 		exit 1
 	fi
 	do_groups $2 $3
+elif [[ "$command" == "media" ]]; then
+	if [[ $# -gt 1 ]]; then
+		echo "Too many arguments provided"
+		exit 1
+	fi
+	do_media
+elif [[ "$command" == "perms" ]]; then
+	if [[ $# -gt 1 ]]; then
+		echo "Too many arguments provided"
+		exit 1
+	fi
+	do_perms
+elif [[ "$command" == "update" ]]; then
+	if [[ $(whoami) != "root" ]]; then
+		echo "Can only run update as root"
+		exit 1
+	fi
+	apt-get update
+	apt-get upgrade
+	apt-get dist-upgrade
+
 else 
-	echo "Unknown command"
+	echo "Unknown command '$command'"
 fi
